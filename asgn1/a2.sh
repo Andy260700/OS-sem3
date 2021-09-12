1a63d361d40f7c87cbcac747b19310bffccb1ccd
Arpan Nandi <arpannandi12@gmail.com>
2021-09-12 23:25:53 +0530

    extra

--- asgn1/a2.sh
+++ asgn1/a2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 read -p 'enter uv1' uv1
 read -p 'enter uv2' uv2
-echo "uv1 is $uv1 and uv2 is $uv2"
+echo "$uv1,$uv2"
+echo "uv1 is $uv2 and uv2 is $uv1"
--- asgn1/a7.sh
+++ asgn1/a7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-i=1
+i=1 #represinting the first command line arg
 for i in {1..10}
 do 
 echo "$1 x $i = $(($i*$1))"
