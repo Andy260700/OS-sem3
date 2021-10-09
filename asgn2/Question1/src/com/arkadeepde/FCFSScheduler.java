69b91659cdf40c21b28de9e9f1309b8a41dfa10b
Arkadeep De <arkadeepde142@gmail.com>
2021-10-09 23:23:22 +0530

    FCFS added

--- asgn2/Question1/src/com/arkadeepde/FCFSScheduler.java
+++ asgn2/Question1/src/com/arkadeepde/FCFSScheduler.java
@@ -5,48 +5,78 @@ import java.util.*;
 
 public class FCFSScheduler implements Scheduler {
 
-    private PriorityQueue<Job> priorityQueue;
+    private ArrayList<Job> jobList;
     private Map<Integer, Integer> endingTimeMap;
-    private Map<Integer, Integer> currentBurstPosition;
 
     public FCFSScheduler(Scanner sc) {
-        priorityQueue = new PriorityQueue<>(new Comparator<Job>() {
-            @Override
-            public int compare(Job o1, Job o2) {
-                if (o1.jobID == o2.jobID)
-                    return 0;
-                if (o1.arrivalTime < o2.arrivalTime) return 1;
-                else if (o1.arrivalTime > o2.arrivalTime) return -1;
-                if (o1.arrivalTime == o2.arrivalTime) {
-                    if (o1.jobID < o2.jobID) return 1;
-                }
-                return -1;
+        PriorityQueue<Job> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            if (o1.jobID == o2.jobID)
+                return 0;
+            if (o1.arrivalTime < o2.arrivalTime) return 1;
+            else if (o1.arrivalTime > o2.arrivalTime) return -1;
+            else {
+                if (o1.jobID < o2.jobID) return 1;
             }
+            return -1;
         });
         endingTimeMap = new HashMap<>();
-        currentBurstPosition = new HashMap<>();
+        jobList = new ArrayList<>();
 
-        while(sc.hasNextInt())
-        {
+        while (sc.hasNextInt()) {
             int jobID = sc.nextInt();
             int priority = sc.nextInt();
-            ArrayList<Integer> burstTimes = new ArrayList<>();
+            int arrivalTime = sc.nextInt();
+            LinkedList<Integer> burstTimes = new LinkedList<>();
+            while (sc.hasNextInt()) {
+                int burstTime = sc.nextInt();
+                if(burstTime == -1) break;
+                burstTimes.add(burstTime);
+            }
+
+            jobList.add(new Job(jobID, priority, arrivalTime, burstTimes));
+            priorityQueue.addAll(jobList);
+        }
+        int time = 0;
+        while(!priorityQueue.isEmpty()) {
+            Job job = priorityQueue.poll();
+            time = Math.max(time, job.arrivalTime);
+            var burstTimes = job.getBurstTimes();
+            int cpuBurst = burstTimes.removeFirst();
+            if(burstTimes.isEmpty()) {
+                endingTimeMap.put(job.jobID, time + cpuBurst);
+            } else {
+                int osBurst = burstTimes.removeFirst();
+                priorityQueue.add(new Job(job.jobID, job.priority, time + cpuBurst + osBurst, burstTimes));
+            }
+            time += cpuBurst;
         }
     }
 
     @Override
     public void turnaroundTime() {
-        System.out.println(endingTimeMap);
+        System.out.println("Job ID\tTAT");
+        for(var job : jobList)
+        {
+            System.out.printf("%d\t\t%d\n", job.jobID, endingTimeMap.get(job.jobID) - job.arrivalTime);
+        }
     }
 
     @Override
     public void completionTime() {
-
+        System.out.println("Job ID\tET");
+        for(var job : jobList)
+        {
+            System.out.printf("%d\t\t%d\n", job.jobID, endingTimeMap.get(job.jobID));
+        }
     }
 
     @Override
     public void waitingTime() {
-
+        System.out.println("Job ID\tWT");
+        for(var job : jobList)
+        {
+            System.out.printf("%d\t\t%d\n", job.jobID, endingTimeMap.get(job.jobID) - job.arrivalTime - job.getBurstTimes().parallelStream().reduce(0, (a, b) -> a + b));
+        }
     }
 
 }
--- asgn2/Question1/src/com/arkadeepde/Job.java
+++ asgn2/Question1/src/com/arkadeepde/Job.java
@@ -1,21 +1,25 @@
 package com.arkadeepde;
 
+
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class Job {
     public final int jobID;
     public final int arrivalTime;
     public final int priority;
-    private ArrayList<Integer> burstTimes;
+    private LinkedList<Integer> burstTimes;
 
-    public Job(int jobID, int arrivalTime, int priority, ArrayList<Integer> burstTimes) {
+    public Job(int jobID, int priority, int arrivalTime, LinkedList<Integer> burstTimes) {
         this.jobID = jobID;
         this.arrivalTime = arrivalTime;
         this.priority = priority;
         this.burstTimes = burstTimes;
     }
 
-    public ArrayList<Integer> getBurstTimes() {
-        return burstTimes;
+    public LinkedList<Integer> getBurstTimes() {
+        return (LinkedList<Integer>) burstTimes.clone();
     }
 }
